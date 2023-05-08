e86d02a94a4f9eaa74f454f3bb183d0bb01d1cfe
Linh Le <53576397+hiolinh@users.noreply.github.com>
2023-05-08 23:21:51 +0700

    feat: add on change

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -1,139 +1,152 @@
 <template>
-    <div>
-        <span :id="prevHandler">
-            <slot name="prev"></slot>
-        </span>
-        <div :id="elementHandle" :class="['owl-carousel', 'owl-theme']">
-            <slot></slot>
-        </div>
-        <span :id="nextHandler">
-            <slot name="next"></slot>
-        </span>
+  <div>
+    <span :id="prevHandler">
+      <slot name="prev"></slot>
+    </span>
+    <div :id="elementHandle" :class="['owl-carousel', 'owl-theme']">
+      <slot></slot>
     </div>
+    <span :id="nextHandler">
+      <slot name="next"></slot>
+    </span>
+  </div>
 </template>
 <script>
-    import 'owl.carousel/dist/assets/owl.carousel.css';
-    import 'owl.carousel/dist/assets/owl.theme.default.css';
-    import 'owl.carousel';
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import "owl.carousel";
 
-    export default {
-      name: 'VOwlCarousel',
-      data: function(){
-        return {
-            owl: null,
-            prevHandler: 'carousel_prev_' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-            elementHandle: 'carousel_' +  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-            nextHandler: 'carousel_next_' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-          }
+export default {
+  name: "VOwlCarousel",
+  data: function () {
+    return {
+      owl: null,
+      prevHandler:
+        "carousel_prev_" +
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15),
+      elementHandle:
+        "carousel_" +
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15),
+      nextHandler:
+        "carousel_next_" +
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15),
+    };
+  },
+  props: {
+    items: {
+      default: 3,
+    },
+    margin: {
+      default: 0,
+    },
+    loop: {
+      default: false,
+    },
+    center: {
+      default: false,
+    },
+    nav: {
+      default: true,
+    },
+    autoplay: {
+      default: false,
+    },
+    autoplaySpeed: {
+      default: false,
+    },
+    rewind: {
+      default: true,
+    },
+    mouseDrag: {
+      default: true,
+    },
+    touchDrag: {
+      default: true,
+    },
+    pullDrag: {
+      default: true,
+    },
+    freeDrag: {
+      default: false,
+    },
+    stagePadding: {
+      default: 0,
+    },
+    autoWidth: {
+      default: false,
+    },
+    autoHeight: {
+      default: false,
+    },
+    dots: {
+      default: true,
+    },
+    autoplayTimeout: {
+      default: 5000,
+    },
+    autoplayHoverPause: {
+      default: false,
+    },
+    responsive: {
+      default() {
+        return {};
       },
-      props: {
-        items : {
-            default: 3
-        },
-        margin : {
-            default: 0
-        },
-        loop : {
-            default: false
-        },
-        center : {
-            default: false
-        },
-        nav : {
-            default: true
-        },
-        autoplay : {
-            default: false
-        },
-        autoplaySpeed : {
-            default: false
-        },
-        rewind : {
-            default: true
-        },
-        mouseDrag : {
-            default: true
-        },
-        touchDrag: {
-            default: true
-        },
-        pullDrag: {
-            default: true
-        },
-        freeDrag: {
-            default: false
-        },
-        stagePadding: {
-            default: 0
-        },
-        autoWidth: {
-            default: false
-        },
-        autoHeight: {
-            default: false
-        },
-        dots: {
-            default: true
-        },
-        autoplayTimeout: {
-            default: 5000
-        },
-        autoplayHoverPause: {
-            default: false
-        },
-        responsive: {
-            default() {
-                return {}
-            }
-        }
-      },
-      watch: {
-        autoplay(nVal) {
-          console.log('watch autoplay', nVal)
-          if (nVal) {
-            console.log('play')
-            this.owl.trigger('play.owl.autoplay', this.autoplayTimeout)
-          } else {
-            console.log('stop')
-            this.owl.trigger('stop.owl.autoplay')
-          }
-        }
-      },
-      mounted : function() {
-        this.owl = $('#' + this.elementHandle).owlCarousel({
-            items        : this.items,
-            margin       : this.margin,
-            loop         : this.loop,
-            center       : this.center,
-            nav          : this.nav,
-            autoplay     : this.autoplay,
-            autoplaySpeed: this.autoplaySpeed,
-            rewind       : this.rewind,
-            mouseDrag    : this.mouseDrag,
-            touchDrag    : this.touchDrag,
-            pullDrag     : this.pullDrag,
-            freeDrag     : this.freeDrag,
-            stagePadding     : this.stagePadding,
-            autoWidth     : this.autoWidth,
-            autoHeight     : this.autoHeight,
-            dots     : this.dots,
-            autoplayTimeout     : this.autoplayTimeout,
-            autoplayHoverPause     : this.autoplayHoverPause,
-            responsive     : this.responsive
-        });
+    },
+  },
+  watch: {
+    autoplay(nVal) {
+      console.log("watch autoplay", nVal);
+      if (nVal) {
+        console.log("play");
+        this.owl.trigger("play.owl.autoplay", this.autoplayTimeout);
+      } else {
+        console.log("stop");
+        this.owl.trigger("stop.owl.autoplay");
+      }
+    },
+  },
+  mounted: function () {
+    this.owl = $("#" + this.elementHandle).owlCarousel({
+      items: this.items,
+      margin: this.margin,
+      loop: this.loop,
+      center: this.center,
+      nav: this.nav,
+      autoplay: this.autoplay,
+      autoplaySpeed: this.autoplaySpeed,
+      rewind: this.rewind,
+      mouseDrag: this.mouseDrag,
+      touchDrag: this.touchDrag,
+      pullDrag: this.pullDrag,
+      freeDrag: this.freeDrag,
+      stagePadding: this.stagePadding,
+      autoWidth: this.autoWidth,
+      autoHeight: this.autoHeight,
+      dots: this.dots,
+      autoplayTimeout: this.autoplayTimeout,
+      autoplayHoverPause: this.autoplayHoverPause,
+      responsive: this.responsive,
+    });
 
-        $('#' + this.prevHandler).click(function() {
-          this.owl.trigger('prev.owl.carousel');
-        });
+    $("#" + this.prevHandler).click(function () {
+      this.owl.trigger("prev.owl.carousel");
+    });
 
-        $('#' + this.nextHandler).click(function() {
-          this.owl.trigger('next.owl.carousel');
-        });
-      },
-      methods: {
-          toSlide(index) {
-              this.owl.trigger('to.owl.carousel', index);
-          }
-      },
-  }
+    $("#" + this.nextHandler).click(function () {
+      this.owl.trigger("next.owl.carousel");
+    });
+
+    this.owl.on('changed.owl.carousel', function(event) {
+        this.$emit('onChange', event)
+    });
+  },
+  methods: {
+    toSlide(index) {
+      this.owl.trigger("to.owl.carousel", index);
+    },
+  },
+};
 </script>
